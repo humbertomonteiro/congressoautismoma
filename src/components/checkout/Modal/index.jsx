657fc21b6758011c818645039a0572976307d8d6
Humberto Monteiro <humbertoaraujox@icloud.com>
2025-02-25 18:03:30 -0300

    att

--- src/components/checkout/Modal/index.jsx
+++ src/components/checkout/Modal/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Modal.module.css";
+import styles from "./modal.module.css";
 
 const Modal = ({ isOpen, onClose, title, message, type, content }) => {
   if (!isOpen) return null;
